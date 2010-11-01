5497a5c6bd6c1bbf788ae89dca78cbdc3a0e2c75
Andres J. Diaz <ajdiaz@connectical.com>
2010-11-01 20:48:49 +0100

    More fixes in setup.py
    Now README file is hardcoded in setup.py, due to bogus behaviour in pypi
    uploads.

--- setup.py
+++ setup.py
@@ -2,14 +2,49 @@
 
 from setuptools import setup, find_packages
 
-NOSY_VERSION="1.0"
+WHISTLER_VERSION="1.0"
 
 setup(
     name = "whistler",
-    version = NOSY_VERSION,
+    version = WHISTLER_VERSION,
     description = "An extensible MUC bot for XMPP.",
-    long_description=open('README.rst').read() + """
-    For more information, point to whistler web.""",
+    long_description="""
+
+============
+Whistler Bot
+============
+
+Whistler Bot is an XMPP bot written in python using pyxmpp, which is
+a requirement. The bot is designed to handle some commands, and it's easy to
+extend.
+
+Simple usage::
+
+    from whistler.bot import WhistlerBot
+
+    bot = WhistlerBot( "myjid@myserver.com", "mypassword" )
+    bot.start()
+
+The provided console script called ``whistler`` is a single bot which reply
+to a ``!ping`` command.
+
+
+Extending bot
+-------------
+
+You can extend the bot functionalities, just see for example the code of the
+whistler console script. In short you can add commands creating a new class
+from WhistlerBot, and define new functions in the form *cmd_* plus the
+command name, for example, to handle the command *ping*::
+
+    from whistler.bot import WhistlerBot
+
+    class MyBot(WhistlerBot):
+
+        def cmd_ping(self, msg, args):
+            return "pong"
+
+""",
     author='Andres J. Diaz',
     author_email='ajdiaz@connectical.com',
     url='http://github.com/ajdiaz/whistler',
