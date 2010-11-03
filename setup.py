1acfd46c002a23949b193c3ae76c1705035bded4
Adrian Perez <aperez@igalia.com>
2010-11-04 01:35:18 +0200

    Read setup.py's long description from README file
    Change setup.py so it loads README.rst as long description, instead of
    having a copy if it hardwired inside the setup script. This avoids having
    a duplicate, and somewhat simplifies setup.py

--- setup.py
+++ setup.py
@@ -1,50 +1,22 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
+from os import path
 
 WHISTLER_VERSION="1.0"
+
+
+def get_file_contents(filename):
+    fd = file(path.join(path.dirname(__file__), filename), "r")
+    content = fd.read()
+    fd.close()
+    return content
 
 setup(
     name = "whistler",
     version = WHISTLER_VERSION,
     description = "An extensible MUC bot for XMPP.",
-    long_description="""
-
-============
-Whistler Bot
-============
-
-Whistler Bot is an XMPP bot written in python using pyxmpp, which is
-a requirement. The bot is designed to handle some commands, and it's easy to
-extend.
-
-Simple usage::
-
-    from whistler.bot import WhistlerBot
-
-    bot = WhistlerBot( "myjid@myserver.com", "mypassword" )
-    bot.start()
-
-The provided console script called ``whistler`` is a single bot which reply
-to a ``!ping`` command.
-
-
-Extending bot
--------------
-
-You can extend the bot functionalities, just see for example the code of the
-whistler console script. In short you can add commands creating a new class
-from WhistlerBot, and define new functions in the form *cmd_* plus the
-command name, for example, to handle the command *ping*::
-
-    from whistler.bot import WhistlerBot
-
-    class MyBot(WhistlerBot):
-
-        def cmd_ping(self, msg, args):
-            return "pong"
-
-""",
+    long_description=get_file_contents("README.rst"),
     author='Andres J. Diaz',
     author_email='ajdiaz@connectical.com',
     url='http://github.com/ajdiaz/whistler',
