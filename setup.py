e90b09f07101790c1080de6293c1831c2ee55722
Andres J. Diaz <ajdiaz@connectical.com>
2013-05-27 20:35:58 +0200

    Improvement documentation.

--- setup.py
+++ setup.py
@@ -29,7 +29,9 @@ setup(
     install_requires = [
         "sleekxmpp>=1.0",
         "pyasn1>=0.1.4",
-        "pyasn1-modules>=0.0.4"
+        "pyasn1-modules>=0.0.4",
+        "twitter>=1.9.4",
+        "bitly-api>=0.2",
     ],
     classifiers=[
           'Development Status :: 4 - Beta',
